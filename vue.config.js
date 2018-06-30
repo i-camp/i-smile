9563e7a7cd9d8244d9870d5bba4ff36bcf7a872f
kubotak-is <kubotak@istyle.co.jp>
2018-07-01 02:28:11 +0900

    add: pwa setup

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,20 @@ module.exports = {
   configureWebpack: {
     plugins: [
     ]
+  },
+  pwa: {
+    name: 'i smail',
+    themeColor: '#FFC107',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+ 
+    // configure the workbox plugin
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      importWorkboxFrom: 'local',
+      include: [/\.html$/, /\.js$/, /\.css$/, /\.png$/, /\.jpeg$/, /\.jpg$/, /\.gif$/],
+      exclude: [/\.json$/, /\.map$/],
+    }
   }
 }
