8bf78bd12fc5e0ab7d8d93bb30e2002e5b5fcb87
kubotak-is <kubotak@istyle.co.jp>
2018-07-01 14:36:16 +0900

    remove: middileware

--- src/routes.js
+++ src/routes.js
@@ -1,59 +1,31 @@
 import VueRouter from 'vue-router'
-import { middleware, createMiddleware } from 'vue-router-middleware'
 
 import Start from '@/components/Start.vue'
-import Login from '@/components/Login.vue'
 import Reading from '@/components/Reading.vue'
 import Generate from '@/components/Generate.vue'
 import Emotiondetection from '@/components/Emotiondetection.vue'
-
-import User from '@/user'
-
-// Pass middleware name and callback function
-createMiddleware('require-auth', (args, to, from, next) => {
-  if (User.hasUser()) {
-    if (to.name === 'login') {
-      next({ path: '/' });
-      return;
-    }
-    next();
-    return;
-  }
-  if (to.name === 'login') {
-    next();
-    return;
-  }
-  next({ path: '/login' });
-});
  
 const routes = [
-  ...middleware('require-auth', [
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
-    },
-    {
-      path: '/',
-      name: 'start',
-      component: Start
-    },
-    {
-      path: '/reading',
-      name: 'reading',
-      component: Reading,
-    },
-    {
-      path: '/generate',
-      name: 'generate',
-      component: Generate,
-    },
-    {
-      path: '/emotion/:uuid',
-      name: 'emotiondetection',
-      component: Emotiondetection
-    },
-  ]),
+  {
+    path: '/',
+    name: 'start',
+    component: Start
+  },
+  {
+    path: '/reading',
+    name: 'reading',
+    component: Reading,
+  },
+  {
+    path: '/generate',
+    name: 'generate',
+    component: Generate,
+  },
+  {
+    path: '/emotion/:uuid',
+    name: 'emotiondetection',
+    component: Emotiondetection
+  },
 ];
  
 const router = new VueRouter({ routes });
